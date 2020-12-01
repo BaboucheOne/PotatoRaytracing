aea78c0a66018970e921d411a12ae57839cf5007
Babouche <clnabergel@hotmail.fr>
2020-11-30 23:31:31 -0500

    Basic obj parser
    Parse basic obj (Not support material)
    Support only triangles.
    Support 2 methods in face:
     - //f v1//vn1 v2//vn2 v3//vn3 --VerticeNormalMethod
     - //f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3 --VerticeTextureNormalMethod

---
+++ PotatoRaytracing/src/Scene/ObjBaker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace PotatoRaytracing
+{
+    public class ObjBaker
+    {
+        private readonly PotatoObjParser potatoObjParser = new PotatoObjParser();
+
+        public ObjBaker()
+        {
+        }
+
+        public void Build(ref List<PotatoMesh> meshes)
+        {
+            if (meshes.Count == 0) return;
+            BakeMeshes(meshes);
+        }
+
+        private void BakeMeshes(List<PotatoMesh> meshs)
+        {
+            for (int i = 0; i < meshs.Count; i++)
+            {
+                BakeMesh(meshs[i]);
+            }
+        }
+
+        private void BakeMesh(PotatoMesh mesh)
+        {
+            potatoObjParser.Parse(ref mesh);
+            mesh.SetPosition(mesh.Position);
+        }
+    }
+}
